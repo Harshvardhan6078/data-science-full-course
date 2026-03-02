4f545e8b467ebc1fd0e9677032ccf82e321a7759
harshvardhan basude <basudeharsh@gmail.com>
2026-03-02 22:12:47 +0530

    Removed embedded repository and cleaned structure

---
+++ kiran academy/python_basics/modules in python/module_in_python.py
@@ -0,0 +1,20 @@
+# # A module in Python is simply a file containing Python code (.py file) that can define:
+# Functions
+# Classes
+# Variables
+# Executable statements
+
+# Modules help in:
+# Code reusability
+# Better organization
+# Separation of concerns
+# Large project structuring
+
+# datetime, math, 
+
+from datetime import datetime
+dt1 = datetime(2026, 1, 12 , 5 , 18 , 3)
+print(dt1)
+print(datetime.now())
+print(datetime.now().date())
+print(datetime.now().time())
---
+++ kiran academy/python_basics/python_exception_handeling.py
@@ -0,0 +1,43 @@
+# def count(file):
+#     f = open(file ,'r')
+#     lines =  f.readlines()
+#     op = len(lines)
+#     return op
+# print(count('C:\Users\Prime\OneDrive\Desktop\kiran academy\python_basics\dl.txt'))
+
+# num1 = eval(input('num1'))
+# num2 = eval(input('num2'))
+
+# sub = num1 - num2
+# add = num1 + num2
+
+# # you can create your own exception
+# class DevisionBy5Error(Exception):
+#     pass
+
+
+
+
+# try:
+#     div = num1/num2
+
+# except ZeroDivisionError:                            #please define eception name here but in default it is e
+#     print("cant devide by 0")
+
+# except NameError:
+#     print('wrong input')
+
+# except:
+#     print('bro something is wrong')
+# else :
+#     print("the code eecute only if try get ecute")
+# finally:
+#     print( 'this code will always eecute i any sitution')
+
+
+
+
+# count no of characters
+
+l = [1,2,3,4,5,6]
+print(l[5])
